caaac83e9dbb4458ba2a6a673f3d15b18491291e
neuronnitin@gmail.com <neuronnitin@gmail.com>
2025-05-04 23:55:50 +0530

    Added Seaborn visualization notebook

--- ai-web-may/aiweb/aiweb/urls.py
+++ ai-web-may/aiweb/aiweb/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aiweb_day1_petapi.urls'))
+
+    #  web application endpoint
+    #path('api/v1/', include('aiweb_day1_petapi.urls')),
+
+    #  API endpoints
+    path('api/v1/', include('aiweb_day1_petapi.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ai-web-may/aiweb/aiweb_day1_petapi/urls.py
+++ ai-web-may/aiweb/aiweb_day1_petapi/urls.py
@@ -1,5 +1,24 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter
+router.register('pets', )
 urlpatterns = [
-    path('pets/', views.get_all_pets, name='get_all_pets'),
+
+
+
+    """
+    
+    # CLASS BASED VIEWS
+    
+    path('pets/', views.Pets.as_view()),
+    path('pets/<int:pk>/', views.PetDetail.as_view())
+    
+    """
+
+
+    # FUNCTION BASED VIEWS
+    #  path('pets/', views.get_all_pets, name='get_all_pets'),
+    #  path('pets/<int:pk>/', views.petDetailView)
 ]
