919fc88baa3bf32089621f3504ddab0db117dd2e
Polar Bear <neuronnitin@gmail.com>
2025-05-05 00:09:30 +0530

    Delete news directory

--- news/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import SignUpView
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
