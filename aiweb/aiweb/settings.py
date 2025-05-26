31e09841934a30a536fcd25548387d1132a15613
neuronnitin@gmail.com <neuronnitin@gmail.com>
2025-05-26 13:59:05 +0530

    Ready for Render deploy

--- aiweb/aiweb/settings.py
+++ aiweb/aiweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r@z#@py&mr+^e_*^1_&1^^yjv%qj-((kw8=z_jtvb4u56=kqn)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
